ca14eebd182194d21808f17c1964428dc7447831
larrygf02 <raulgf_02@hotmail.com>
2019-03-30 23:22:52 -0500

    Configuracion inicial

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server escucha en puerto 3000');
+//routes
+
+//settings
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+//middlewares
+
+
+//starting server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 })
