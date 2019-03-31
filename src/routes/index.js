64389c36fcfe902be8634c6cbeb12cd39cdf5a3c
larrygf02 <raulgf_02@hotmail.com>
2019-03-31 01:02:32 -0500

    Agregando datos al modelo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+//importando los models
+const Task = require('../models/task');
+
 router.get('/', (req, res) => {
     res.render('index')
+})
+
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.send('Recibido')
 })
 
 module.exports = router;
